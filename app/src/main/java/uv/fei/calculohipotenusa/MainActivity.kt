e45d7ef9124cd73b4b1a2a7ebdb901796ca15507
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2021-10-13 17:30:25 -0500

    Funcionalidades: Limpiar formulario, Validar formulario y Calcular hipotenusa

--- app/src/main/java/uv/fei/calculohipotenusa/MainActivity.kt
+++ app/src/main/java/uv/fei/calculohipotenusa/MainActivity.kt
@@ -2,10 +2,69 @@ package uv.fei.calculohipotenusa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonCalcular : Button = findViewById(R.id.btn_Calcular)
+        botonCalcular.setOnClickListener {
+            if(validarFormulario()) {
+                calcularHipotenusa()
+            }
+        }
+        val botonLimpiar : Button = findViewById(R.id.btn_Limpiar)
+        botonLimpiar.setOnClickListener {
+            limpiarFormulario()
+        }
     }
+
+    fun calcularHipotenusa() {
+        val txt_catetoOpuesto : EditText = findViewById(R.id.txt_CatetoOpuesto)
+        val catOpuesto = txt_catetoOpuesto.text.toString().toDouble();
+
+        val txt_catetoAdyacente : EditText = findViewById(R.id.txt_CatetoAdyacente)
+        val catAdyacente = txt_catetoAdyacente.text.toString().toDouble()
+
+        var resultado : Double = Math.sqrt(Math.pow(catOpuesto, catOpuesto) + Math.pow(catAdyacente, catAdyacente))
+
+        val lbl_Resultado : TextView = findViewById(R.id.lbl_Resultado)
+        lbl_Resultado.setText("Resultado: " + resultado);
+    }
+
+    fun validarFormulario(): Boolean {
+        var resultado = false
+        var mensaje = ""
+
+        val txt_catetoOpuesto : EditText = findViewById(R.id.txt_CatetoOpuesto)
+        val catOpuesto = txt_catetoOpuesto.text.toString().toDoubleOrNull();
+
+        val txt_catetoAdyacente : EditText = findViewById(R.id.txt_CatetoAdyacente)
+        val catAdyacente = txt_catetoAdyacente.text.toString().toDoubleOrNull()
+
+        if (catOpuesto != null && catAdyacente != null) {
+            resultado = true
+        } else {
+            val mensaje = "Campos vacios: " +
+                    "${ if (catOpuesto == null) {"\n   Cateto opuesto" } else {""}} " +
+                    "${if(catAdyacente == null) {"\n   Cateto adyacente"} else {""}}"
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+            resultado = false;
+        }
+
+        return resultado
+    }
+
+    fun limpiarFormulario() {
+        val txt_catetoOpuesto : EditText = findViewById(R.id.txt_CatetoOpuesto)
+        txt_catetoOpuesto.setText("")
+
+        val txt_catetoAdyacente : EditText = findViewById(R.id.txt_CatetoAdyacente)
+        txt_catetoAdyacente.setText("")
+    }
+
 }
