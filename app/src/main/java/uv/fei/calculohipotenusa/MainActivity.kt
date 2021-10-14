2cdb0d20f82e115903d8821509bca62d491942bb
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2021-10-14 15:32:47 -0500

    Se agregan los comentarios a las funciones

--- app/src/main/java/uv/fei/calculohipotenusa/MainActivity.kt
+++ app/src/main/java/uv/fei/calculohipotenusa/MainActivity.kt
@@ -23,27 +23,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Se realiza el calculo de la hipotenusa de un triángulo rectangulo y se muestra el resultado en pantalla.
+     */
     fun calcularHipotenusa() {
         val txt_catetoOpuesto : EditText = findViewById(R.id.txt_CatetoOpuesto)
         val catOpuesto = txt_catetoOpuesto.text.toString().toDouble();
-
         val txt_catetoAdyacente : EditText = findViewById(R.id.txt_CatetoAdyacente)
         val catAdyacente = txt_catetoAdyacente.text.toString().toDouble()
-
         var resultado : Double = Math.sqrt(Math.pow(catOpuesto, 2.0) + Math.pow(catAdyacente, 2.0))
-        print(resultado)
-
         var lbl_Resultado : TextView = findViewById(R.id.lbl_Resultado)
         lbl_Resultado.setText("Resultado: " + resultado);
     }
 
+    /**
+     * Valida que en los campos no esten vacios y sean numeros.
+     */
     fun validarFormulario(): Boolean {
         var resultado = false
         var mensaje = ""
-
         val txt_catetoOpuesto : EditText = findViewById(R.id.txt_CatetoOpuesto)
         val catOpuesto = txt_catetoOpuesto.text.toString().toDoubleOrNull();
-
         val txt_catetoAdyacente : EditText = findViewById(R.id.txt_CatetoAdyacente)
         val catAdyacente = txt_catetoAdyacente.text.toString().toDoubleOrNull()
 
@@ -60,6 +60,9 @@ class MainActivity : AppCompatActivity() {
         return resultado
     }
 
+    /**
+     * Vacia los campos de texto del formulario.
+     */
     fun limpiarFormulario() {
         val txt_catetoOpuesto : EditText = findViewById(R.id.txt_CatetoOpuesto)
         txt_catetoOpuesto.setText("")
